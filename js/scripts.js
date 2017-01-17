d4dd4eaec2ec21482afe7b52af0227dc75870b70
philip putnam <putnamp989@gmail.com>
2017-01-17 11:33:19 -0800

    add input handling for user output in scripts.js using if branching

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,22 @@
 $(document).ready(function() {
   $("#count-up form").submit(function(event) {
+    $(".results").text(" ");
     var userCountTo = parseInt($("input#count-to").val());
     var userCountBy = parseInt($("input#count-by").val());
 
-    for (var index = 0; index <= userCountTo; index += userCountBy) {
-      $(".results").append(index + " ");
+    if (!userCountTo) {
+      alert("Please enter a number to count to!")
+    }
+    else if (!userCountBy || userCountBy > userCountTo) {
+      alert("Please enter a number that is smaller than what you want to count to!")
+    }
+    else {
+      for (var index = 0; index <= userCountTo; index += userCountBy) {
+        $(".results").append(index + " ");
+      }
     }
 
 
-
-
-    // var usrResults = [];
-    // usrResults.push(index);
 
     event.preventDefault();
 
